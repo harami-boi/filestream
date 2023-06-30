8b13a07201ae600c9e12e91db14b14f919596627
Mrstark69 <112131793+Mrstark69@users.noreply.github.com>
2023-06-30 08:21:56 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -13,7 +13,7 @@ from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
 db = Database(Var.DATABASE_URL, Var.name)
 from pyrogram.types import ReplyKeyboardMarkup
 
-if Var.MY_PASS:
+if MY_PASS:
     buttonz = ReplyKeyboardMarkup(
         [
             ["start⚡️", "help📚", "login🔑", "DC"],
