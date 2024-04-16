c58e92b45ec2c70cdf67d29665b671013ebf7b7a
HaRami_b0i <39855010+harami-boi@users.noreply.github.com>
2024-04-16 22:37:43 +0530

    Update sh_url.py

--- sh_url.py
+++ sh_url.py
@@ -2,7 +2,7 @@ import requests
 shrt_url = "https://atglinks.com/api?api=99ebb69ac0cfdc42674ba2f27c8de6976ee60020&url="
 urls = []
 def shorten_url(url_list):
-  if stream_url is not None and download_url is not None:
+  if url_list[0] is not None and url_list[1] is not None:
     for url in url_list:
       res = requests.get(f'{shrt_url}{url}')
       if res.status_code == 200:
